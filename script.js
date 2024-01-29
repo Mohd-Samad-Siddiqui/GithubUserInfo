bea894f87bd2049d481342de61936798281035ba
Mohd-Samad-Siddiqui <samad.siddiqui47@gmail.com>
2024-01-29 17:34:26 +0000

    created XMLHTTPRequest and it's working fine

--- script.js
+++ script.js
@@ -17,11 +17,30 @@ const userRepo = document.querySelector('#repos-link');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log(userInput.value);
-  if(userInput.value === ''){
+  let userHandleInput = userInput.value;
+  if(userHandleInput === ''){
     document.getElementById("user-id").placeholder = "Enter user_id first";
   }
   else{
     document.getElementById("user-id").placeholder = "Github user_id";
-  }
+    fetchUserData(userHandleInput);
+  }  
 })
+
+function fetchUserData(username){
+  let xhr = new XMLHttpRequest();
+  let url = "https://api.github.com/users/" + username;
+  console.log(url);
+  xhr.open('GET', url);
+  xhr.onload = function() {
+    if(xhr.status === 200){
+      console.log("Im inside 200 status code")
+      let userData = JSON.parse(xhr.responseText);
+      console.log("User Data", userData);
+    }
+    else{
+      console.log(xhr.status);
+    }
+  }
+  xhr.send();
+}
