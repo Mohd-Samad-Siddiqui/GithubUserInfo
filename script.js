dd6e54c9cdf8b5cbd750171ecda85b7f5a01968b
Mohd-Samad-Siddiqui <samad.siddiqui47@gmail.com>
2024-01-30 10:07:30 +0000

    Complete the userDisplay Card (Iff user exists)

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const form = document.querySelector('form');
 const userInput = document.querySelector('input');
 const submitBtn = document.querySelector('#submit');
 const userCard = document.querySelector('#user-card');
-const userImg = document.querySelector('#img');
+const userImg = document.querySelector('#userImg');
 const userName = document.querySelector('#name');
 const userId = document.querySelector('#id');
 const joinedData = document.querySelector('#joined-date');
@@ -11,10 +11,23 @@ const reposNum = document.querySelector('#repos-number');
 const followersNum = document.querySelector('#followers-number');
 const followingNum = document.querySelector('#following-number');
 const userLocation = document.querySelector('#place');
-const userHandle = document.querySelector('#handle');
+const userTwitterHandle = document.querySelector('#handle');
 const userMail = document.querySelector('#mail');
 const userRepo = document.querySelector('#repos-link');
-let userData;
+let userHandleInput, userData;
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  userHandleInput = userInput.value;
+  if(userHandleInput === ''){
+    document.getElementById("user-id").placeholder = "Enter user_id first";
+  }
+  else{
+    document.getElementById("user-id").placeholder = "Github user_id";
+    fetchUserData(userHandleInput);
+    userInput.value = '';
+  }  
+})
 
 function fetchUserData(username){
   const xhr = new XMLHttpRequest();
@@ -24,6 +37,7 @@ function fetchUserData(username){
     if(xhr.status === 200){
       userData = JSON.parse(xhr.responseText);
       console.log(userData);
+      displayCard();
     }
     else{
       console.log(xhr.status);
@@ -32,15 +46,46 @@ function fetchUserData(username){
   xhr.send();
 }
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  let userHandleInput = userInput.value;
-  if(userHandleInput === ''){
-    document.getElementById("user-id").placeholder = "Enter user_id first";
+const dateConvertor = (createdDate) => {
+  const originalDate = new Date(createdDate);
+  const dateString = originalDate.toDateString();
+  const [day, month, date, year] = dateString.split(" ");
+  return `${date} ${month} ${year}`;
+}
+
+function displayCard(){
+  userImg.src = userData.avatar_url;
+  userName.innerHTML = userData.name;
+  userId.innerHTML = userData.login;
+  userId.href = userData.html_url;
+  const date = dateConvertor(userData.created_at);
+  joinedData.innerHTML = date;
+  bio.innerHTML = userData.bio !== null ? userData.bio : "";
+  reposNum.innerHTML = userData.public_repos;
+  followersNum.innerHTML = userData.followers;
+  followingNum.innerHTML = userData.following;
+
+  if(userData.location){
+    userLocation.innerHTML = userData.location; 
+  }else{
+    userLocation.innerHTML = "Not Available";
+    userLocation.parentElement.style.opacity = 0.3;
   }
-  else{
-    document.getElementById("user-id").placeholder = "Github user_id";
-    fetchUserData(userHandleInput);
-    console.log(userData);
-  }  
-})
+  
+  if(userData.twitter_username){
+    userTwitterHandle.innerHTML = userData.twitter_username; 
+  }else{
+    userTwitterHandle.innerHTML = "Not Available";
+    userTwitterHandle.parentElement.style.opacity = 0.3;
+  }
+
+  if(userData.mail){
+    userMail.innerHTML = userData.mail; 
+  }else{
+    userMail.innerHTML = "Not Available";
+    userMail.parentElement.style.opacity = 0.3;
+  }
+
+  userRepo.href = userData.html_url + "?tab=repositories";
+  userCard.style.display = 'block';
+}
